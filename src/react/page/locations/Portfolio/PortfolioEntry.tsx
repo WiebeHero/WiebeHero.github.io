fdc73f3c34807867d9d2f3b732d5b1333127dd90
Wiebe Sportel <50573980+WiebeHero@users.noreply.github.com>
2025-04-26 11:38:08 +0200

    Patch 0.0.401
    = Changed sizing in list

--- src/react/page/locations/Portfolio/PortfolioEntry.tsx
+++ src/react/page/locations/Portfolio/PortfolioEntry.tsx
@@ -8,7 +8,7 @@ interface PortfolioEntryProps{
 function PortfolioEntry({asset, last}: PortfolioEntryProps) {
     return (
         <>
-            <div className={`w-50 min-w-450p max-w-1000p max-h-300p bg-800 border border-3 ${last ? "" : "border-bottom-0"} border-800 border-secondary d-flex flex-row align-items-start justify-content-start position-relative`}>
+            <div className={`w-50 min-w-400p max-w-1000p max-h-300p bg-800 border border-3 ${last ? "" : "border-bottom-0"} border-800 border-secondary d-flex flex-row align-items-start justify-content-start position-relative`}>
                 <img className={"w-25"} src={asset.GetEntry("Thumbnail")} alt={"Not found"}/>
                 <div className={"w-100 d-flex flex-column align-items-center"}>
                     <p className={"h3 text-white fs-12px0_5vw20px m-0"}>{asset.GetEntry("Title")}</p>
